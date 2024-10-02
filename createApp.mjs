d97f1eef64c638c340bdbc6d8760f649384b77db
solmaz-safilkani <solmaz.safikhani2004@gmail.com>
2024-10-02 21:58:55 +0330

    add tutorial code

---
+++ createApp.mjs
@@ -0,0 +1,30 @@
+import express from "express";
+import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import passport from "passport";
+import MongoStore from "connect-mongo";
+import mongoose from "mongoose";
+import "./strategies/local-strategy.mjs";
+export function createApp() {
+	const app = express();
+	app.use(express.json());
+	app.use(cookieParser("helloworld"));
+	app.use(
+		session({
+			secret: "anson the dev",
+			saveUninitialized: true,
+			resave: false,
+			cookie: {
+				maxAge: 60000 * 60,
+			},
+			store: MongoStore.create({
+				client: mongoose.connection.getClient(),
+			}),
+		})
+	);
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.use(routes);
+	return app;
+}
---
+++ index.mjs
@@ -0,0 +1,20 @@
+import express from 'express';
+const app = express();
+
+
+
+// import mongoose from "mongoose";
+// import { createApp } from "./createApp.mjs";
+
+// mongoose
+// 	.connect("mongodb://localhost/express_tutorial")
+// 	.then(() => console.log("Connected to Database"))
+// 	.catch((err) => console.log(`Error: ${err}`));
+
+// const app = createApp();
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`running on port ${PORT}`);
+});
